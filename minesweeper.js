de441c629acd4b5bf6be45057edbf51a8473119d
Meaghan De Klerk <meaghan.dk@gmail.com>
2019-03-06 14:47:25 +1300

    adding you win and #1 stretch

--- minesweeper.js
+++ minesweeper.js
@@ -146,11 +146,38 @@ function startGame () {
   lib.initBoard()
 }
 
+document.addEventListener("click", checkForWin);
+
+
 // Define this function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
+  var countOfMarkedMines = 0;
+  var countOfTotalMines = 0;
+
+  for (var i = 0; i < board.cells.length; i++) {
+    if (board.cells[i].isMine === true) {
+      countOfTotalMines++;
+    }
+    if (board.cells[i].isMine === true && board.cells[i].isMarked === true) {
+      countOfMarkedMines++;
+    }
+    if (board.cells[i].hidden === true && board.cells[i].isMine === false) {
+      return;
+    }
+  }
+
+  if (countOfMarkedMines === countOfTotalMines) {
+    lib.displayMessage('You Win!!');
+  } else {
+    return;
+  }
+
+
+
+
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
@@ -169,11 +196,44 @@ function checkForWin () {
 
 function countSurroundingMines (cell) {
   var countMines = 0;   //creating loop
+
   var surroundingCells = lib.getSurroundingCells(cell.row, cell.col)  //accessing array
+
   for (var i = 0; i < surroundingCells.length; i++){  // if surronding cells is mine add 1 
+
     if (surroundingCells[i].isMine === true){   // counts if is mine
+
       countMines++;
     }
   }
   return countMines; 
 }
+
+// stretch
+
+//easier way to make cells
+
+// makeCells();
+
+// function Cell(rowNum, colNum, mine) {
+//   this.row = rowNum;
+//   this.col = colNum;
+//   this.isMine = mine;
+//   this.isMarked = false;
+//   this.hidden = true;
+// }
+
+// function makeCells() {
+//   board.cells.push(new Cell(0, 0, false));
+//   board.cells.push(new Cell(0, 1, false));
+//   board.cells.push(new Cell(0, 2, false));
+      //....
+//   board.cells.push(new Cell(1, 0, false));
+//   board.cells.push(new Cell(1, 1, true));
+//   board.cells.push(new Cell(1, 2, true));
+      //...
+//   board.cells.push(new Cell(2, 0, false));
+//   board.cells.push(new Cell(2, 1, true));
+//   board.cells.push(new Cell(2, 2, false));
+// }
+
